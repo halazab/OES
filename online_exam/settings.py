6d2e5a682db49fb51bc8ed24a46c3e58761b9cdd
halazab <136014860+halazab@users.noreply.github.com>
2025-02-21 19:55:20 +0300

    Update settings.py

--- online_exam/settings.py
+++ online_exam/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,4 +138,5 @@ BASE_URL = 'https://oes-nk2r.onrender.com/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
